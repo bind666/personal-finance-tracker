710bb98ced1ae941a7bb2e98d7b316b20a21095a
bind666 <144540630+bind666@users.noreply.github.com>
2025-06-01 09:40:32 +0530

    frontend link added in backend cors

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ limit: "1MB", extended: true }));
 app.use(cookieParser())
 // app.use(cors());
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://personal-finance-tracker-rho-lime.vercel.app"],
     credentials: true,
 }))
 
